7d76360cc3669ff09aed577da0f9333190588ae0
Yunus <yyavuz1999@gmail.com>
2026-02-14 02:11:43 +0100

    add push to talk

--- Assets/Scripts/Core/Controls/VRButtonInput.cs
+++ Assets/Scripts/Core/Controls/VRButtonInput.cs
@@ -8,6 +8,9 @@ public class VRButtonInput : MonoBehaviour
     [SerializeField] private Button stopButton;
     [SerializeField] private NPCController npcController;
 
+    private const OVRInput.Button TalkInputButton = OVRInput.Button.One;
+    private const OVRInput.Button StopInputButton = OVRInput.Button.Two;
+
     private void Awake()
     {
         if (npcController == null)
@@ -18,14 +21,19 @@ public class VRButtonInput : MonoBehaviour
 
     void Update()
     {
-        // A button on right controller
-        if (OVRInput.GetDown(OVRInput.Button.One))
+        // A button on right controller (push-to-talk: press to start, release to stop)
+        if (OVRInput.GetDown(TalkInputButton))
         {
-            talkButton?.onClick.Invoke();
+            InvokeTalkButton();
+        }
+
+        if (OVRInput.GetUp(TalkInputButton))
+        {
+            InvokeTalkButton();
         }
 
         // B button on right controller
-        if (OVRInput.GetDown(OVRInput.Button.Two))
+        if (OVRInput.GetDown(StopInputButton))
         {
             if (stopButton != null)
             {
@@ -35,6 +43,14 @@ public class VRButtonInput : MonoBehaviour
             {
                 npcController?.StopCurrentSpeech();
             }
+        }
+    }
+
+    private void InvokeTalkButton()
+    {
+        if (talkButton != null && talkButton.IsInteractable())
+        {
+            talkButton.onClick.Invoke();
         }
     }
 }
